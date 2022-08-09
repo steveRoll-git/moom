5333d793a71f84ce2ae2a9bd5e8ee2a941792168
Ron Tseytlin <ronts390@gmail.com>
2022-08-09 17:54:09 +0300

    added code to main.rs to get rid of deadcode warnings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+use lang::Parser;
+
+use crate::vm::VM;
+
 mod lang;
 mod vm;
 
 fn main() {
+    //TODO this code is temporary, and is mainly here to get rid of deadcode warnings
+    let code = "
+    func main() {
+        print(\"moom works!\");
+    }";
+    let mut parser = Parser::new(Box::new(code.chars()), "code".to_string(), None);
+    let program = parser.parse_file();
+    match program {
+        Ok(program) => {
+            let mut vm: VM = VM::new(program, None);
+            match vm.run() {
+                Ok(_) => {}
+                Err(error) => {
+                    panic!("Runtime Error:\n{}", error)
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+            panic!();
+        }
+    }
 }
